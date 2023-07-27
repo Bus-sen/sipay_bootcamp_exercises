62ae924247486f1bb9fa7f1b38e77a5136e178e2
Büşra Şen <snbusra66@gmail.com>
2023-07-27 13:17:33 +0300

    week1 exercise was done

---
+++ sipay_bootcamp_exercise1/sipay_bootcamp_exercise1/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sipay_bootcamp_cohorts_homework1.Models;
+
+namespace sipay_bootcamp_cohorts_homework1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private static List<Product> ProductList = new List<Product>()
+    {
+        new Product()
+        {
+            Id = 1,
+            Title = "Asus",
+            Model = "VivoBook X1502ZA-EJ1066AS",
+            Price = 15099
+        },
+        new Product()
+        {
+            Id = 2,
+            Title = "Lenovo",
+            Model = "Thinkbook 16 21CY004RTX02",
+            Price = 38008
+        },
+        new Product()
+        {
+            Id = 3,
+            Title = "Dell",
+            Model = "Latitude 3520-17",
+            Price = 29806
+        },
+    };
+
+    //GET
+    [HttpGet]
+    public List<Product> GetProducts()
+    {
+        return ProductList;
+    }
+
+    //GET
+    [HttpGet("{id}")]
+    public Product GetById(int id)
+    {
+        var product = ProductList.Where(product => product.Id == id).SingleOrDefault();
+        return product;
+    }
+
+    //POST
+    [HttpPost]
+    public IActionResult AddProduct([FromBody] Product newProduct)
+    {
+        var product = ProductList.SingleOrDefault(x => x.Title == newProduct.Title);
+
+        if (product != null)
+            return BadRequest();
+
+        ProductList.Add(newProduct);
+        return Ok();
+    }
+
+    //PUT
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(int id, [FromBody] Product updatedProduct)
+    {
+        var product = ProductList.SingleOrDefault(x => x.Id == id);
+
+        if (product == null)
+            return BadRequest();
+
+        product.Title = updatedProduct.Title != default ? updatedProduct.Title : product.Title;
+        product.Model = updatedProduct.Model != default ? updatedProduct.Model : product.Model;
+        product.Price = updatedProduct.Price != default ? updatedProduct.Price : product.Price;
+        return Ok();
+    }
+
+    //PATCH
+    [HttpPatch("{id}")]
+    public IActionResult UpdateProductTitle(int id, [FromBody] Product updatedTitle)
+    {
+        var product = ProductList.SingleOrDefault(x => x.Id == id);
+
+        if (product == null)
+            return BadRequest();
+
+        product.Title = updatedTitle.Title != default ? updatedTitle.Title : product.Title;
+        return Ok();
+    }
+
+    //DELETE
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProduct(int id)
+    {
+        var product = ProductList.SingleOrDefault(x => x.Id == id);
+
+        if (product == null)
+            return BadRequest();
+
+        ProductList.Remove(product);
+        return Ok();
+    }
+
+    //SORT
+    [HttpGet("sort")]
+    public IActionResult GetSortProduct(string sortBy)
+    {
+        if (sortBy == "title")
+        {
+            var sortedProduct = ProductList.OrderBy(title => title.Title); //ascending sort by title
+            return Ok(sortedProduct);
+        }
+        else if (sortBy == "price")
+        {
+            var sortedProduct = ProductList.OrderBy(price => price.Price); //ascending sort by price
+            return Ok(sortedProduct);
+        }
+        return BadRequest();
+    }
+
+    //FILTER
+    [HttpGet("list")]
+    public IActionResult GetProductByTitle([FromQuery] string title)
+    {
+        var filteredProduct = ProductList.Where(p => p.Title.Contains(title)).ToList();
+        return Ok(filteredProduct);
+    }
+}
---
+++ sipay_bootcamp_exercise1/sipay_bootcamp_exercise1/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace sipay_bootcamp_exercise1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
